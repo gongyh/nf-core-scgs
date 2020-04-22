2f578a9a730efe7f99b8d9b3ce83dbc4751f897b
gongyh <gongyh@qibebt.ac.cn>
2020-04-22 17:10:33 +0800

    add R script to draw probability density curves

---
+++ bin/plotProp.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+
+# Command line argument processing
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) < 2) {
+  stop("Usage: plotProp.R <input_per-base_coverage> <prefix>", call.=FALSE)
+}
+
+input_fn <- args[1]
+output_prefix <- args[2]
+
+# Load / install packages
+if (!require("magicaxis")) {
+  install.packages("magicaxis", dependencies=TRUE, repos='http://cloud.r-project.org/')
+  library("magicaxis")
+}
+
+data <- read.table(input_fn, header=F)
+d <- density(data$V1)
+
+pdf(paste0(output_prefix,"_pdrc.pdf"), useDingbats = FALSE, width = 6, height = 6)
+
+plot(d, main=paste0("PDRC-",output_prefix), xlab="Relative Coverage", ylab="Probability Density", 
+     lwd=2, log="x", xlim=c(0.1,10), axes=FALSE, col="red",xaxs="i",yaxs="i")
+magaxis(1:4)
+
+dev.off()
