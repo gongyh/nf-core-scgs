ec69e9f5e0767fce6c2961b3ab0054b5692c8db4
gongyh <gongyh@qibebt.ac.cn>
2023-07-20 10:57:59 +0800

    reformart

--- bin/doubletd_haploid.py
+++ bin/doubletd_haploid.py
@@ -15,9 +15,22 @@ import numpy as np
 from scipy.stats import beta as scibeta
 
 
-class doubletFinder():
-
-    def __init__(self, df_total, df_alt, cells, delta, beta, mu_hom, alpha_fp, alpha_fn, missing=False, verbose=True, binom=False, precision=None):
+class doubletFinder:
+    def __init__(
+        self,
+        df_total,
+        df_alt,
+        cells,
+        delta,
+        beta,
+        mu_hom,
+        alpha_fp,
+        alpha_fn,
+        missing=False,
+        verbose=True,
+        binom=False,
+        precision=None,
+    ):
 
         self.df_total = df_total
         self.df_alt = df_alt
@@ -40,14 +53,12 @@ class doubletFinder():
         print(f"number of position is {npos}")
 
         if (self.df_total.values - self.df_alt.values).min() < 0:
-            raise Exception(
-                'total reads must be greater than or equal to alternate reads!')
+            raise Exception("total reads must be greater than or equal to alternate reads!")
 
-        self.Sigma = ((0, 1), (0, 1/2, 1))
-        self.Theta = ((0, 1), (0, 1/2, 1))
+        self.Sigma = ((0, 1), (0, 1 / 2, 1))
+        self.Theta = ((0, 1), (0, 1 / 2, 1))
 
-        self.px_z = {x: 0 for z in [0, 1] for x in itertools.product(
-            self.muts, self.Sigma[z], [z])}
+        self.px_z = {x: 0 for z in [0, 1] for x in itertools.product(self.muts, self.Sigma[z], [z])}
 
         if self.alpha_fn == None or self.alpha_fp == None or self.precision == None:
             # get vaf numpy array
@@ -64,8 +75,7 @@ class doubletFinder():
                 self.alpha_fn = 1 - mean2
 
             if self.precision == None:
-                mean3, prec3 = getBetaMOM(
-                    vaf_values[(vaf_values > 0.15) & (vaf_values < 0.85)])
+                mean3, prec3 = getBetaMOM(vaf_values[(vaf_values > 0.15) & (vaf_values < 0.85)])
                 self.precision = np.median([prec1, prec2, prec3])
 
         print(f"alpha_fn = {self.alpha_fn}")
@@ -82,7 +92,7 @@ class doubletFinder():
 
                 reads = self.df_alt[m]
                 total = self.df_total[m]
-                vaf = pd.DataFrame(reads/total)
+                vaf = pd.DataFrame(reads / total)
 
                 loh_cells = vaf.loc[vaf[m] >= 0.85]
                 wt_cells = vaf.loc[vaf[m] <= 0.15]
@@ -97,34 +107,33 @@ class doubletFinder():
                 self.px_z[m, 0, 0] = 1 - self.mu_hom
                 self.px_z[m, 1, 0] = self.mu_hom
 
-        #norm_const = {}
-        #for m in self.muts:
-        #    for a, b in itertools.product(self.Sigma[0], repeat=2):
-        #        c = (a + b)/2
-        #        if c in self.Sigma[1]:
-        #            self.px_z[m, c, 1] += self.px_z[m, a, 0] * \
-        #                self.px_z[m, b, 0]
-        #    norm_const[m] = sum([self.px_z[m, a, 0] * self.px_z[m, b, 0]
-         #                       for a, b in itertools.product(self.Sigma[0], repeat=2)])
+            # norm_const = {}
+            # for m in self.muts:
+            #    for a, b in itertools.product(self.Sigma[0], repeat=2):
+            #        c = (a + b)/2
+            #        if c in self.Sigma[1]:
+            #            self.px_z[m, c, 1] += self.px_z[m, a, 0] * \
+            #                self.px_z[m, b, 0]
+            #    norm_const[m] = sum([self.px_z[m, a, 0] * self.px_z[m, b, 0]
+            #                       for a, b in itertools.product(self.Sigma[0], repeat=2)])
 
-       # for m in self.muts:
-       #     for c in self.Sigma[1]:
-       #         self.px_z[m, c, 1] /= norm_const[m]
+            # for m in self.muts:
+            #     for c in self.Sigma[1]:
+            #         self.px_z[m, c, 1] /= norm_const[m]
 
             self.px_z[m, 0, 1] = est_wt_rate * 0.25
             self.px_z[m, 1, 1] = est_loh_rate * 0.25
-            self.px_z[m, 1/2, 1] = 0.75
+            self.px_z[m, 1 / 2, 1] = 0.75
 
-        self.py_xz = {x: 0 for z in [0, 1] for x in itertools.product(
-            self.Theta[z], self.Sigma[z], [z])}
+        self.py_xz = {x: 0 for z in [0, 1] for x in itertools.product(self.Theta[z], self.Sigma[z], [z])}
 
-        self.py_xz[0,   0,   0] = 1 - self.beta
-        self.py_xz[1,   1,   0] = 1 - self.beta
-        self.py_xz[0,   0,   1] = 1 - self.beta**2
-        self.py_xz[0,   1/2, 1] = self.beta * (1 - self.beta)
-        self.py_xz[1/2, 1/2, 1] = (1-self.beta)**2
-        self.py_xz[1,   1/2, 1] = self.beta * (1 - self.beta)
-        self.py_xz[1,   1,   1] = 1 - self.beta**2
+        self.py_xz[0, 0, 0] = 1 - self.beta
+        self.py_xz[1, 1, 0] = 1 - self.beta
+        self.py_xz[0, 0, 1] = 1 - self.beta**2
+        self.py_xz[0, 1 / 2, 1] = self.beta * (1 - self.beta)
+        self.py_xz[1 / 2, 1 / 2, 1] = (1 - self.beta) ** 2
+        self.py_xz[1, 1 / 2, 1] = self.beta * (1 - self.beta)
+        self.py_xz[1, 1, 1] = 1 - self.beta**2
 
         self.doublet_result = None
 
@@ -144,9 +153,9 @@ class doubletFinder():
             self.logprobs[cell, 1] = self.prv_z(cell, 1)
 
             if self.logprobs[cell, 1] - self.logprobs[cell, 0] > self.threshold:
-                self.doublet_result[cell] = 'doublet'
+                self.doublet_result[cell] = "doublet"
             else:
-                self.doublet_result[cell] = 'singlet'
+                self.doublet_result[cell] = "singlet"
 
     def prv_z(self, cell, z):
 
@@ -171,7 +180,7 @@ class doubletFinder():
 
     def prv_y_b(self, r, v, y):
         yprime = self.alpha_fp + (1 - self.alpha_fp - self.alpha_fn) * y
-        return nCr(r+v, v) * (yprime ** v) * ((1-yprime) ** r)
+        return nCr(r + v, v) * (yprime**v) * ((1 - yprime) ** r)
 
     def prv_y_bb(self, r, v, y):
         yprime = self.alpha_fp + (1 - self.alpha_fp - self.alpha_fn) * y
@@ -179,35 +188,35 @@ class doubletFinder():
             yprime = 0.001
         if y == 1:
             yprime = 0.999
-        #print("y = " + str(y))
-        alpha = self.precision*yprime
-        #print("alpha = " + str(alpha))
+        # print("y = " + str(y))
+        alpha = self.precision * yprime
+        # print("alpha = " + str(alpha))
         beta = self.precision - alpha
-        #print("beta = " + str(beta))
+        # print("beta = " + str(beta))
         n = r + v
         vaf = v / n
         if vaf == 0:
             vaf = 0.01
         if vaf == 1:
             vaf = 0.99
-        #print(f"n:{n} r:{r} v:{v} p:{y} alpha:{alpha} beta:{beta}")
+        # print(f"n:{n} r:{r} v:{v} p:{y} alpha:{alpha} beta:{beta}")
         prob = scibeta.pdf(vaf, alpha, beta)
         return prob
 
     def writeSolution(self, outputFile):
 
-        with open(outputFile, 'w') as output:
+        with open(outputFile, "w") as output:
             output.write("cell_id\tprob_z0\tprob_z1\tprediction\n")
             for cell in self.cells:
                 output.write(
-                    f"{cell}\t{self.logprobs[cell, 0]}\t{self.logprobs[cell,1]}\t{self.doublet_result[cell]}\n")
+                    f"{cell}\t{self.logprobs[cell, 0]}\t{self.logprobs[cell,1]}\t{self.doublet_result[cell]}\n"
+                )
 
     def likelihood(self):
         likelihood = len(self.cells) * math.log(1 - self.delta)
         for cell in self.cells:
-            if self.doublet_result[cell] == 'doublet':
-                likelihood += self.logprobs[cell, 1] - \
-                    self.logprobs[cell, 0] - self.threshold
+            if self.doublet_result[cell] == "doublet":
+                likelihood += self.logprobs[cell, 1] - self.logprobs[cell, 0] - self.threshold
         return likelihood
 
 
@@ -215,32 +224,30 @@ def getBetaMOM(x):
 
     m_x = np.mean(x)
     s_x = np.std(x)
-    x_alpha = m_x*((m_x*(1 - m_x)/s_x**2) - 1)
-    x_beta = (1 - m_x)*((m_x*(1 - m_x)/s_x**2) - 1)
+    x_alpha = m_x * ((m_x * (1 - m_x) / s_x**2) - 1)
+    x_beta = (1 - m_x) * ((m_x * (1 - m_x) / s_x**2) - 1)
 
-    return x_alpha/(x_alpha + x_beta), x_alpha + x_beta
+    return x_alpha / (x_alpha + x_beta), x_alpha + x_beta
 
 
 def nCr(n, r):
     f = math.factorial
-    #print(n, r)
-    return f(n) // f(r) // f(n-r)
+    # print(n, r)
+    return f(n) // f(r) // f(n - r)
 
 
 def main(args):
 
     df_total = pd.read_csv(args.inputTotal)
     df_alt = pd.read_csv(args.inputAlternate)
-    cells = list(df_total['cell_id'].values)
-    df_total = df_total.set_index(['cell_id'])
-    df_alt = df_alt.set_index(['cell_id'])
+    cells = list(df_total["cell_id"].values)
+    df_total = df_total.set_index(["cell_id"])
+    df_alt = df_alt.set_index(["cell_id"])
     vaf_values = (df_alt / df_total).values
-    valid_cols = np.logical_and(
-        np.any(vaf_values > 0.85, axis=0), np.any(vaf_values < 0.15, axis=0))
+    valid_cols = np.logical_and(np.any(vaf_values > 0.85, axis=0), np.any(vaf_values < 0.15, axis=0))
     valid_list = valid_cols.tolist()
     df_total = df_total.loc[:, valid_list]
     df_alt = df_alt.loc[:, valid_list]
-    
 
     if len(df_total) != len(df_alt):
         raise Exception("number of cells in the two input files do not match!")
@@ -253,9 +260,20 @@ def main(args):
     if args.verbose:
         print(f"number of cells is {ncells}")
 
-    solver = doubletFinder(df_total, df_alt, cells, delta=args.delta, beta=args.beta, missing=args.missing,
-                           mu_hom=args.mu_hom, alpha_fp=args.alpha_fp, alpha_fn=args.alpha_fn,
-                           verbose=args.verbose, binom=args.binom, precision=args.prec)
+    solver = doubletFinder(
+        df_total,
+        df_alt,
+        cells,
+        delta=args.delta,
+        beta=args.beta,
+        missing=args.missing,
+        mu_hom=args.mu_hom,
+        alpha_fp=args.alpha_fp,
+        alpha_fn=args.alpha_fn,
+        verbose=args.verbose,
+        binom=args.binom,
+        precision=args.prec,
+    )
 
     solver.solve()
 
@@ -265,47 +283,51 @@ def main(args):
 def str2bool(v):
     if isinstance(v, bool):
         return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ("yes", "true", "t", "y", "1"):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    elif v.lower() in ("no", "false", "f", "n", "0"):
         return False
     else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+        raise argparse.ArgumentTypeError("Boolean value expected.")
 
 
 def get_options():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--inputTotal", type=str,
-                        help="csv file with a table of total read counts for each position in each cell")
-    parser.add_argument("--inputAlternate", type=str,
-                        help="csv file with a table of alternate read counts for each position in each cell")
-    parser.add_argument("--delta", type=float, default=0.1,
-                        help="expected doublet rate [0.1]")
-    parser.add_argument("--beta", type=float, default=0.05,
-                        help="allelic dropout (ADO) rate [0.05]")
-    parser.add_argument("--mu_hom", type=float,
-                        help="homozygous mutation rate [None]")
-    parser.add_argument("--alpha_fp", type=float,
-                        help="copy false positive error rate [None]")
-    parser.add_argument("--alpha_fn", type=float,
-                        help="copy false negative error rate [None]")
-    parser.add_argument("-o", "--outputfile", type=str,
-                        help="output file name")
-    parser.add_argument("--noverbose", dest="verbose",
-                        help="do not output statements from internal solvers [default is false]", action='store_false')
-    parser.add_argument("--binomial", dest="binom",
-                        help="use binomial distribution for read count model [default is false]", action='store_true')
-    parser.add_argument("--prec", type=float,
-                        help="precision for beta-binomial distribution [None]")
-    parser.add_argument("--missing", dest="missing",
-                        help="use missing data in the model? [No]", action='store_true')
+    parser.add_argument(
+        "--inputTotal", type=str, help="csv file with a table of total read counts for each position in each cell"
+    )
+    parser.add_argument(
+        "--inputAlternate",
+        type=str,
+        help="csv file with a table of alternate read counts for each position in each cell",
+    )
+    parser.add_argument("--delta", type=float, default=0.1, help="expected doublet rate [0.1]")
+    parser.add_argument("--beta", type=float, default=0.05, help="allelic dropout (ADO) rate [0.05]")
+    parser.add_argument("--mu_hom", type=float, help="homozygous mutation rate [None]")
+    parser.add_argument("--alpha_fp", type=float, help="copy false positive error rate [None]")
+    parser.add_argument("--alpha_fn", type=float, help="copy false negative error rate [None]")
+    parser.add_argument("-o", "--outputfile", type=str, help="output file name")
+    parser.add_argument(
+        "--noverbose",
+        dest="verbose",
+        help="do not output statements from internal solvers [default is false]",
+        action="store_false",
+    )
+    parser.add_argument(
+        "--binomial",
+        dest="binom",
+        help="use binomial distribution for read count model [default is false]",
+        action="store_true",
+    )
+    parser.add_argument("--prec", type=float, help="precision for beta-binomial distribution [None]")
+    parser.add_argument("--missing", dest="missing", help="use missing data in the model? [No]", action="store_true")
     parser.set_defaults(missing=False)
     parser.set_defaults(binom=False)
     parser.set_defaults(verbose=True)
 
-    args = parser.parse_args(None if sys.argv[1:] else ['-h'])
+    args = parser.parse_args(None if sys.argv[1:] else ["-h"])
 
-    return(args)
+    return args
 
 
 def main_cli():
--- bin/vcf_csv.py
+++ bin/vcf_csv.py
@@ -3,8 +3,9 @@ import pysam
 
 # read VCF file
 def read_vcf_file(vcf_path):
-    vcf_reader = vcf.Reader(open(vcf_path, 'r'))
+    vcf_reader = vcf.Reader(open(vcf_path, "r"))
     return list(vcf_reader)
+
 
 # get all mutant sites
 def get_variants_positions(vcf_file):
@@ -15,16 +16,17 @@ def get_variants_positions(vcf_file):
         positions.append(chrom_pos)
     return positions
 
+
 # write AD.tsv header
 def write_ad_header(ad_file, positions):
-    ad_file.write('cell_id,{}\n'.format(','.join(positions)))
+    ad_file.write("cell_id,{}\n".format(",".join(positions)))
 
 
 # write AD.tsv data
 def write_ad_data(ad_file, vcf_file, positions, counter):
     vcf_records = read_vcf_file(vcf_file)
     # init row
-    row = ['0'] * len(positions)
+    row = ["0"] * len(positions)
     #  row
     for record in vcf_records:
         chrom_pos = f"{record.CHROM}:{record.POS}"
@@ -32,24 +34,24 @@ def write_ad_data(ad_file, vcf_file, positions, counter):
         # get variant depth
         sample_record = record.samples[counter]
         sample_name = sample_record.sample
-        if (sample_record['AD']):
-            depth = sample_record['AD'][1]
+        if sample_record["AD"]:
+            depth = sample_record["AD"][1]
             row[index] = str(depth)
     # write row
     print(sample_name + "   AD       write!")
-    ad_file.write('{},{}\n'.format(sample_name, ','.join(row)))
+    ad_file.write("{},{}\n".format(sample_name, ",".join(row)))
 
 
 # write DP.tsv header
 def write_dp_header(dp_file, positions):
-    dp_file.write('cell_id,{}\n'.format(','.join(positions)))
+    dp_file.write("cell_id,{}\n".format(",".join(positions)))
 
 
 # write DP.tsv data
 def write_dp_data(dp_file, vcf_file, positions, counter):
     vcf_records = read_vcf_file(vcf_file)
     # init row
-    row = ['0'] * len(positions)
+    row = ["0"] * len(positions)
     # write row
     for record in vcf_records:
         chrom_pos = f"{record.CHROM}:{record.POS}"
@@ -57,12 +59,12 @@ def write_dp_data(dp_file, vcf_file, positions, counter):
         # get variant depth
         sample_record = record.samples[counter]
         sample_name = sample_record.sample
-        if (sample_record['DP']):
-            depth = sample_record['DP']
+        if sample_record["DP"]:
+            depth = sample_record["DP"]
             row[index] = str(depth)
     # write row
     print(sample_name + "  DP   write!")
-    dp_file.write('{},{}\n'.format(sample_name, ','.join(row)))
+    dp_file.write("{},{}\n".format(sample_name, ",".join(row)))
 
 
 # main
@@ -80,6 +82,6 @@ def main(vcf_file, ad_file, dp_file):
 
 
 # call main function
-vcf_file = 'monovar.vcf'
-with open('AD.csv', 'w') as ad_file, open('DP.csv', 'w') as dp_file:
+vcf_file = "monovar.vcf"
+with open("AD.csv", "w") as ad_file, open("DP.csv", "w") as dp_file:
     main(vcf_file, ad_file, dp_file)
